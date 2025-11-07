663abecddece4043d82191292d9f4749bb44a4df
VVatchful <tdbt1122@gmail.com>
2025-11-07 04:58:38 +0000

    worked on data csv function

--- csv_reader/csv_reader.c
+++ csv_reader/csv_reader.c
@@ -1,3 +1,4 @@
+#include <asm-generic/errno-base.h>
 #include <stdlib.h>
 #include <stdio.h>
 #include <stdbool.h>
@@ -34,14 +35,27 @@ struct parserResults {
 
 int stock_data_csv() {
     const char *filename = "../CSV/all_stocks_combined.csv";
-    FILE *file = fopen("../CSV/all_stocks_combined.csv", "r");
+    FILE *file = fopen(filename, "r");
     if (file == NULL) {
-        fprintf(stderr, "Error opening CSV file: %s : %s/n", filename, strerror(errno));
-        fclose(file);
-        return 0;
-    } else {
-        perror("Persmission was denied or could not be opened. Please check your file perms:");
-        return 0;
+        if (errno == ENOENT) {
+            printf("Couldn't find the file: %s\n", filename);
+            return -1;
+        } else if (errno == EACCES) {
+            printf("Permission was denied for the file: %s\n", filename);
+            return -1;
+        } else {
+            printf("Could not open file: %s\n", filename);  // Fixed typo!
+            return -1;
+        }
     }
     
+    int stock_array = allocate_stock();
+    if (stock_array == NULL) {
+        fclose(file);
+        printf("Memory allocation failed for stock array\n");
+        return -1; 
+    
+    
+    fclose(file);
+    return 0;
 }
