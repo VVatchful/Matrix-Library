b81ecd585445d119cfbeb017b2cd8d5d853f31f2
VVatchful <tdbt1122@gmail.com>
2025-11-11 05:42:46 +0000

    fixed related bugs to csv structs and functions

--- csv_reader/csv_reader.c
+++ csv_reader/csv_reader.c
@@ -1,4 +1,3 @@
-#include <asm-generic/errno-base.h>
 #include <stdlib.h>
 #include <stdio.h>
 #include <stdbool.h>
@@ -14,24 +13,39 @@ struct stockRecord {
     float low_price;
     float close_price;
     long int volume;
-    char original_data_string;
+    char *original_data_string;
 };
 
 struct stockData {
-    struct stockData* records;
+    struct stockRecord* records;
     int size;
     int capacity;
 };
 
-#define INITIAL_CAPACITY = 100;
-#define GROWTH_FACTOR = 2;
+#define INITIAL_CAPACITY 100
+#define GROWTH_FACTOR 2
 
 struct parserResults {
     bool success;
-    char error_message;
-    char tokens;
+    char error_message[256];
+    char tokens[256];
     int line_number;
 };
+
+struct stockData* allocate_stock() {
+    struct stockData* container = malloc(sizeof(struct stockData));
+    if (container == NULL) {
+        return NULL;
+    }
+    container->records = malloc(INITIAL_CAPACITY * sizeof(struct stockRecord));
+    if (container->records == NULL) {
+        free(container);
+        return NULL;    
+    }
+    container->size = 0;
+    container->capacity = INITIAL_CAPACITY;
+    return container;
+}
 
 int stock_data_csv() {
     const char *filename = "../CSV/all_stocks_combined.csv";
@@ -44,18 +58,25 @@ int stock_data_csv() {
             printf("Permission was denied for the file: %s\n", filename);
             return -1;
         } else {
-            printf("Could not open file: %s\n", filename);  // Fixed typo!
+            printf("Could not open file: %s\n", filename);
             return -1;
         }
     }
     
-    int stock_array = allocate_stock();
+    struct stockData* stock_array = allocate_stock();
     if (stock_array == NULL) {
         fclose(file);
         printf("Memory allocation failed for stock array\n");
-        return -1; 
+        return -1;
+    }
     
+    // TODO: Read and parse the file
+    // TODO: Add records to stock_array
     
+    free(stock_array->records);
+    free(stock_array);
     fclose(file);
+    
     return 0;
 }
+
